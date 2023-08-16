16091f963a6106370888c8e434989b525590a493
crazyDeveloperz <133535122+crazyDeveloperz@users.noreply.github.com>
2023-08-16 03:27:02 -0700

    Update test.py

--- plugins/test.py
+++ plugins/test.py
@@ -10,7 +10,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQ
 from pyrogram.errors.exceptions.bad_request_400 import AccessTokenExpired, AccessTokenInvalid
 from pyrogram.errors import FloodWait
 from config import Config
-from script import Script
+from script import script
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
